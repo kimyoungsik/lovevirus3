6d2445b043ce52af108c90fc23d7c79612fe68d2
dean <dean4208@gmail.com>
2013-07-25 21:31:20 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lovevirus3::Application.config.secret_token = '3e84e7d1c9e97117c7f4127bbe1fb691427e2ed8db771741707a9b6c1068fd00f6b68addaecc2d48aef84452518762fe41da863731bb4c4d2aae410e4b03eb91'
